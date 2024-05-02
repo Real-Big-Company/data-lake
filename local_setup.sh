c31edfa296533f4ad5a22215514db344ce7fd7ea
Liam Bigelow <40188355+bglw@users.noreply.github.com>
2024-05-03 11:03:49 +1200

    Update symlink naming

--- local_setup.sh
+++ local_setup.sh
@@ -2,5 +2,4 @@
 
 # This file sets up symlinks locally so a data lake can be tested 
 
-[ ! -e rbc ] && ln -s ../rbc rbc
-[ ! -e rbc-liminal ] && ln -s ../rbc-liminal rbc-liminal
+[ ! -e liminal ] && ln -s ../liminal liminal
