59c6654f4ac2aa93b477f9363e4745666caec469
Liam Bigelow <40188355+bglw@users.noreply.github.com>
2024-05-06 11:06:02 +1200

    Merge in division / url data

--- local_setup.sh
+++ local_setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 # This file sets up symlinks locally so a data lake can be tested 
+# On CloudCannon, this will use Site Mounting.
 
 [ ! -e liminal ] && ln -s ../liminal liminal
+[ ! -e static ] && ln -s ../static static
